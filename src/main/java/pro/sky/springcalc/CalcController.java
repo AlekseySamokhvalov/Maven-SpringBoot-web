e99f465bb377bd5d28cb1349bec57179f0b66710
AlekseySamokhvalov <6560@mail.ru>
2023-08-18 15:09:06 +0300

    – Реализованы все методы. – Корректная работа с параметрами (наличие и отсутствие). – Корректная работа с аннотациями. – Правильно применена архитектура MVC (нет вычислений в контроллере).

--- src/main/java/pro/sky/springcalc/CalcController.java
+++ src/main/java/pro/sky/springcalc/CalcController.java
@@ -1,6 +1,6 @@
 package pro.sky.springcalc;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +10,24 @@ public class CalcController {
     public CalcController(CalcService calcService){
         this.calcService = calcService;
     }
-//    private CalcService calcService = new CalcService();
-    @GetMapping
-    public String hello(){
-        return calcService.hello();
+    @RequestMapping(path = "/calculator")
+    public String calculator() {
+        return calcService.calculator();
     }
-    @GetMapping(path = "/hello")
-    public String answerHello(@RequestParam("name") String userName){
-        return calcService.answerHello(userName);
+    @RequestMapping(path = "/calculator/plus")
+    public String calculatorPlus(@RequestParam (required = false) Integer num1, Integer num2) {
+        return calcService.calculatorPlus(num1,num2);
+    }
+    @RequestMapping(path = "/calculator/minus")
+    public String calculatorMinus(@RequestParam (required = false) Integer num1, Integer num2) {
+        return calcService.calculatorMinus (num1,num2);
+    }
+    @RequestMapping(path = "/calculator/multiply")
+    public String calculatorMultiply(@RequestParam (required = false) Integer num1, Integer num2) {
+        return calcService.calculatorMultiply(num1,num2);
+    }
+    @RequestMapping(path = "/calculator/divide")
+    public String calculatorDivide(@RequestParam (required = false) Double num1, Double num2) {
+        return calcService.calculatorDivide(num1,num2);
     }
 }
