d3ac195fa08cbe2f0b0afbeed2bd33460a033699
AlekseySamokhvalov <6560@mail.ru>
2023-08-19 11:54:12 +0300

    Добавил @RequestMapping("/calculator") над названием класса. Добавил @RequestParam на проверку, что переданы все параметры.  @RequestParam поставил перед каждым принимаемым параметром метода: public String calculatorPlus(@RequestParam Integer num1, @RequestParam Integer num2)

--- src/main/java/pro/sky/springcalc/CalcController.java
+++ src/main/java/pro/sky/springcalc/CalcController.java
@@ -5,29 +5,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalcController {
     private final CalcService calcService;
     public CalcController(CalcService calcService){
         this.calcService = calcService;
     }
-    @RequestMapping(path = "/calculator")
+    @RequestMapping(path = "/")
     public String calculator() {
         return calcService.calculator();
     }
-    @RequestMapping(path = "/calculator/plus")
-    public String calculatorPlus(@RequestParam (required = false) Integer num1, Integer num2) {
+    @RequestMapping(path = "/plus")
+    public String calculatorPlus(@RequestParam Integer num1, @RequestParam Integer num2) {
         return calcService.calculatorPlus(num1,num2);
     }
-    @RequestMapping(path = "/calculator/minus")
-    public String calculatorMinus(@RequestParam (required = false) Integer num1, Integer num2) {
+    @RequestMapping(path = "/minus")
+    public String calculatorMinus(@RequestParam Integer num1, @RequestParam Integer num2) {
         return calcService.calculatorMinus (num1,num2);
     }
-    @RequestMapping(path = "/calculator/multiply")
-    public String calculatorMultiply(@RequestParam (required = false) Integer num1, Integer num2) {
+    @RequestMapping(path = "/multiply")
+    public String calculatorMultiply(@RequestParam Integer num1, @RequestParam Integer num2) {
         return calcService.calculatorMultiply(num1,num2);
     }
-    @RequestMapping(path = "/calculator/divide")
-    public String calculatorDivide(@RequestParam (required = false) Double num1, Double num2) {
+    @RequestMapping(path = "/divide")
+    public String calculatorDivide(@RequestParam Double num1, @RequestParam Double num2) {
         return calcService.calculatorDivide(num1,num2);
     }
 }
